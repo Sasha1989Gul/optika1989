81eaa8e2d896ccae2ae55bae45e0aabfa64b3699
Sasha1989Gul <118977314+Sasha1989Gul@users.noreply.github.com>
2022-12-06 18:19:28 +0000

    optika1989

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        {/* <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
@@ -20,7 +20,31 @@ function App() {
           >
             Learn React
           </a>
-        </p>
+        </p> */}
+<h1>Hello, React</h1>
+
+<div className="class_info">
+
+	    <p>ФИО: Гулевский А.В.</p>
+      <p>Тел.: 952 592 20 30</p>
+      <p>Эл. почта: 89optika@mail.ru</p>
+    
+</div>
+
+<img src="react.png" />
+
+<div className="class_name">
+
+<p>Город: Липецк</p>
+<p>Страна: Россия</p>
+<p>Количесто жителей:  496,45 тыс.</p>
+<p>Город: Липецк</p>
+<img src="Lipetsk.png" />
+<p>Страна: Россия</p>
+<img src="russia.jpg" />
+
+</div>
+        
       </header>
     </div>
   );
