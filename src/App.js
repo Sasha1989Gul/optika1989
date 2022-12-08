dcd48ae11388c64dfa9891e012433576e432bd27
Sasha1989Gul <118977314+Sasha1989Gul@users.noreply.github.com>
2022-12-08 18:27:03 +0000

    pactice 1

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
 import './App.css';
+import { Bibliography } from './pz08/Bibliography'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p> */}
-<h1>Hello, React</h1>
-
-<div className="class_info">
-
-	    <p>ФИО: Гулевский А.В.</p>
-      <p>Тел.: 952 592 20 30</p>
-      <p>Эл. почта: 89optika@mail.ru</p>
-    
-</div>
-
-<img src="react.png" />
-
-<div className="class_name">
-
-<p>Город: Липецк</p>
-<p>Страна: Россия</p>
-<p>Количесто жителей:  496,45 тыс.</p>
-<p>Город: Липецк</p>
-<img src="Lipetsk.png" />
-<p>Страна: Россия</p>
-<img src="russia.jpg" />
-
-</div>
-        
-      </header>
+    <div>
+      <p>fgrigre</p>
+      <Bibliography ></Bibliography>
     </div>
   );
 }
---
+++ src/pz08/Bibliography.js
@@ -0,0 +1,16 @@
+
+
+function Bibliography() {
+    return (
+        <div>
+            
+            <Bibliography argument="argument_value">
+            <h1> Уильяма Шекспира</h1>
+
+            </Bibliography>
+            
+        </div>
+    );
+}
+
+export {Bibliography};
